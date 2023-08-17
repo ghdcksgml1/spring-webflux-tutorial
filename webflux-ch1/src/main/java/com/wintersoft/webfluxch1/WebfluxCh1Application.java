421e3ab841a5cfb6d7e04cf48d5128efdb30ae0a
ghdcksgml1 <ghdcksgml2@naver.com>
2023-08-17 15:02:12 +0900

    setting(ch1): initial setting

---
+++ webflux-ch1/src/main/java/com/wintersoft/webfluxch1/WebfluxCh1Application.java
@@ -0,0 +1,13 @@
+package com.wintersoft.webfluxch1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxCh1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxCh1Application.class, args);
+	}
+
+}
---
+++ webflux-ch1/src/test/java/com/wintersoft/webfluxch1/WebfluxCh1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wintersoft.webfluxch1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCh1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
